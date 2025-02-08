2ac4773ce5ec56c14092b3f78405ae3325b46557
uriel_salguero <urysalguero09@gmail.com>
2025-02-08 11:57:15 -0300

    dom

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
+  resolve: {
+    alias: {
+      "react-router-dom": "react-router-dom",
+    },
+  },
 });
